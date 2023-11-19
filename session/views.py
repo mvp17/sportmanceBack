bc9a6ab58bed571aa7a0207e2c594bb6b4ad6187
mvp17 <mvisapascual@gmail.com>
2023-11-19 16:37:03 +0100

    Delete authentication failed exception import

--- session/views.py
+++ session/views.py
@@ -1,4 +1,3 @@
-from rest_framework.exceptions import AuthenticationFailed
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from devicesKeyWords.models import DevicesKeyWords
